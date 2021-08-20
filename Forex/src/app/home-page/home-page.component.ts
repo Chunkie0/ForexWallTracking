26d65974d26d1abe9a85b79a93ec8a9f86847c10
Chunkie0 <davisdambis1@gmail.com>
2021-08-20 23:22:11 +0300

    added tables, style and gathering of currency data

--- Forex/src/app/home-page/home-page.component.ts
+++ Forex/src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-home-page',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
+  results: any;
+  userInput = { currency: 'EUR/USD', apiKey: '', interval: 2 }
+  userInputForm!: FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.userInputForm = new FormGroup({
+      currency: new FormControl(this.userInput.currency, [Validators.minLength(7)]),
+      apiKey: new FormControl(this.userInput.apiKey),
+      interval: new FormControl(this.userInput.interval)
+    })
+  }
+
+  onSubmit() {
+    if (this.input.apiKey.value === "") {
+      setInterval(() => {
+      this.fetchCurrency(this.input.currency.value)
+    }, this.userInput.interval * 1000)
+    } else {
+      setInterval(() => {
+        this.fetchCurrency(this.input.currency.value, this.input.apiKey.value)
+      }, this.userInput.interval * 1000)
+    }
+  }
+
+  get input() { return this.userInputForm.controls }
+
+  private async fetchCurrency(currency: string, API_KEY: string = '696cb5103decbd1a9cc7a96dffd0ee73'): Promise<any> {
+    const getCurrency = await fetch(`https://financialmodelingprep.com/api/v3/fx/${currency.replace('/', '')}?apikey=${API_KEY}`)
+    getCurrency.json().then(data => {
+      this.results = data
+    })
   }
 
 }
