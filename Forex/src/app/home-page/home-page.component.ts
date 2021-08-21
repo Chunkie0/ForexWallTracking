9b17daa024509f8075beea1958bcecb669a4fe13
Chunkie0 <davisdambis1@gmail.com>
2021-08-22 02:48:17 +0300

    Added clear interval

--- Forex/src/app/home-page/home-page.component.ts
+++ Forex/src/app/home-page/home-page.component.ts
@@ -10,9 +10,9 @@ export class HomePageComponent implements OnInit {
   results: any;
   userInput = { firstCurrency: '', secondCurrency: '', interval: 2 };
   userInputForm!: FormGroup;
-  currencyArray: any[] = [];
+  currencyString: string = "";
   notInitialized: boolean = true;
-  error: boolean = false;
+  currencyInterval: any;
   constructor() { }
 
   ngOnInit(): void {
@@ -24,31 +24,30 @@ export class HomePageComponent implements OnInit {
   }
 
   onSubmit() {
-
-    this.currencyArray.unshift([this.input.firstCurrency.value, this.input.secondCurrency.value].join(''))
-
+    this.currencyString = this.input.firstCurrency.value + this.input.secondCurrency.value
+    
     if (this.notInitialized) {
-      this.fetchCurrency(this.currencyArray[0]);
+      this.fetchCurrency(this.currencyString);
       this.notInitialized = false;
     }
 
-    if (this.currencyArray.length == 2) { this.currencyArray.pop() }
+    clearInterval(this.currencyInterval)
 
-    if (this.error) {
-      setInterval(() => {
-        this.fetchCurrency(this.currencyArray[0])
-      }, this.input.interval.value * 1000)
-    }
+    this.currencyInterval = setInterval(() => {
+      this.fetchCurrency(this.currencyString)
+    }, this.input.interval.value * 1000)
   }
 
   get input() { return this.userInputForm.controls }
 
   private async fetchCurrency(currency: string): Promise<any> {
-    const getCurrency = await fetch(`https://financialmodelingprep.com/api/v3/fx/${currency}?apikey=ae1fbf49d3a1c7c5111b74a1486776df`)
+    
+    const getCurrency = await fetch(`https://financialmodelingprep.com/api/v3/fx/${currency}?apikey=696cb5103decbd1a9cc7a96dffd0ee73`)
     getCurrency.json().then(data => {
       this.results = data
       if (this.results[0].ticker === null) {
-        this.error = true
+        this.notInitialized = true
+        clearInterval(this.currencyInterval)
         alert("No such currency pair available. Try different one!:)")
       }
     })
